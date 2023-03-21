dac15cb24647c3be6f8552b4f2f387e409b66f76
Gauravtripathii <gauravtripathii7979@gmail.com>
2023-03-21 16:30:14 +0530

    forgot password

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css';
-import '../components/Footer/main.css';
+import '../components/Footer/main.css';   // footer css
+import './forgot-password/main.css';    // forgot password css
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
