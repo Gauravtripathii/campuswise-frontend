b0714c078277bd2c9d3bf43569398b676fc899e1
Gauravtripathii <gauravtripathii7979@gmail.com>
2023-03-21 16:24:24 +0530

    footer final

---
+++ src/components/Footer/static.js
@@ -0,0 +1,20 @@
+export default {
+    text: [
+      "We would love to hear your feedback!",
+      "This helps us improve more!",
+      "Mail us at : kiitconnect@gmail.com",
+    ],
+  
+    links: {
+      head: "Links",
+      members: ["Home", "Team"],
+    },
+  
+    feedback_area: {
+      label: "Or send us a message",
+      placeholder:
+        "Send us any thought of yours, or any feedback or anything you wish to say! We are alll ears.",
+    },
+  
+    made: 'Made with <3 in India'
+  };
